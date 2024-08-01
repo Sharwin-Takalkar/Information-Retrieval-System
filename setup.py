8ac09da64039914f9c46491a0787258d1181e094
Sharwin Takalkar <takalkarsharwin1342002@gmail.com>
2024-08-01 23:13:37 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Information Retrieval System',
+    version= '0.0.0',
+    author= 'Sharwin Takalkar',
+    author_email= 'takalkarsharwin@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
